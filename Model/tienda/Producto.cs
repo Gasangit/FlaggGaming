aac4a581cd7b2b353590fd8b9027c76dad0d0ecf
Gasangit <gasan9git@gmail.com>
2024-10-23 22:49:05 -0300

    vista lista hardware

--- Model/tienda/Producto.cs
+++ Model/tienda/Producto.cs
@@ -27,18 +27,5 @@ namespace FlaggGaming.Model.tienda
 
         public virtual Tienda IdTiendaNavigation { get; set; } = null!;
     }
-
-
-    /*public int idInternoProducto {  get; set; }
-    public int idTienda { get; set; }
-    public int idCategoria {  get; set; }
-    public Tienda tienda {  get; set; }
-    public Categoria categoria { get; set; }
-    public string skuTienda { get; set; }
-    public string descripcion {  get; set; }
-    public string marca {  get; set; }
-    public double precioVenta {  get; set; }
-    public bool estatus {  get; set; }
-    public int? descTienda { get; set; }*/
 }
 
