d6c93361d857f284de4553756b40488b7611da3d
Gasangit <gasan9git@gmail.com>
2024-12-04 22:03:04 -0300

    actualizacion usuario logueado

---
+++ Model/usuarios/UsuarioLogueado.cs
@@ -0,0 +1,21 @@
+﻿namespace FlaggGaming.Model.usuarios
+{
+    public class UsuarioLogueado
+    {
+        public int Id { get; set; } = 0;
+        public string nombre { get; set; } = "";
+        public string apellido { get; set; } = "";
+        public string mail { get; set; } = "";
+        public string password { get; set; } = "";
+        public bool rolTienda { get; set; } = false;
+        
+        public UsuarioLogueado(int Id, string nombre, string apellido, string mail, bool rolTienda) 
+        {
+            this.Id = Id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.mail = mail;
+            this.rolTienda = rolTienda;
+        }
+    }
+}
--- Services/ServicioUsuario/ServicioUsuarioLogin.cs
+++ Services/ServicioUsuario/ServicioUsuarioLogin.cs
@@ -2,34 +2,61 @@
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
 using FlaggGaming.Model.usuarios;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace FlaggGaming.Services.ServicioUsuario;
 
 public class ServicioUsurioLogin
 {
+    public UsuarioLogueado usuarioLogueado { get; set; }
+    ProtectedSessionStorage _ProtectedSessionStorage;
+
     public static int Id { get; set; } = 0;
+    public int IdServicio { get; set; } = 0;
     public static string nombre { get; set; } = "";
     public string nombreServicio { get; set; } = "";
     public static string apellido { get; set; } = "";
+    public string apellidoServicio { get; set; } = "";
     public static string mail { get; set; } = "";
+    public string mailServicio { get; set; } = "";
     public static string password { get; set; } = "";
+    public string passwordServicio { get; set; } = "";
     public static bool rolTienda { get; set; } = false;
+    public bool rolTiendaServicio { get; set; } = false;
+    public bool IsPrerendering { get; set; }
     public event Action OnUsuarioLogin;
+    //private static int anChange;
 
-    public void setUsuarioLoginData(int idParam, string nombreParam, string apellidoParam, string mailParam, bool rolParam)
+    public ServicioUsurioLogin(ProtectedSessionStorage protectedSessionStoreInject)
     {
+        _ProtectedSessionStorage = protectedSessionStoreInject;
+    }
+
+    public async void setUsuarioLoginData(int idParam, string nombreParam, string apellidoParam, string mailParam, bool rolParam)
+    {
+        this.IdServicio = idParam;
+        this.nombreServicio = nombreParam;
+        this.apellidoServicio = apellidoParam;
+        this.mailServicio = mailParam;
+        this.rolTiendaServicio = rolParam;
+
         ServicioUsurioLogin.Id = idParam;
         ServicioUsurioLogin.nombre = nombreParam;
-        this.nombreServicio = nombreParam;
         ServicioUsurioLogin.apellido = apellidoParam;
         ServicioUsurioLogin.mail = mailParam;
         ServicioUsurioLogin.rolTienda = rolParam;
+
+        //anChange++;
+        usuarioLogueado = new UsuarioLogueado(idParam, nombreParam, apellidoParam, mailParam, rolParam);
+        await _ProtectedSessionStorage.SetAsync("usuarioLogueado",usuarioLogueado);
+        System.TimeSpan.FromSeconds(3);
         OnUsuarioLogin?.Invoke();
     }
 
     //public void forzarCambio() { OnUsuarioLogin?.Invoke(); }
 
-    public void setUsuarioLogOut()
+    public async void setUsuarioLogOut()
     {
         ServicioUsurioLogin.Id = 0;
         ServicioUsurioLogin.nombre = "";
@@ -37,7 +64,16 @@ public class ServicioUsurioLogin
         ServicioUsurioLogin.apellido = "";
         ServicioUsurioLogin.mail = "";
         ServicioUsurioLogin.rolTienda = false;
+
+        await _ProtectedSessionStorage.DeleteAsync("usuarioLogueado");
+        System.TimeSpan.FromSeconds(3);
         OnUsuarioLogin?.Invoke();
 
+    }
+
+    public void prerenderDetected(bool estado)
+    {
+        this.IsPrerendering = estado;
+        OnUsuarioLogin?.Invoke();
     }
 }
